8bc9c092ab059acea2fc069be3bb313a7831eaf9
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-09-05 08:19:31 +0530

    initial commit

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    private String name;
+    private long rollno;
+
+    public Student(String name, long rollno) {
+        this.name = name;
+        this.rollno = rollno;
+    }
+}
