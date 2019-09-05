b32ccea812cd0c583da7eb0dbb59d75dac439b85
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-09-05 08:32:25 +0530

    adding getter and setter

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -3,6 +3,19 @@ public class Rectangle {
     private double breadth;
 
     public Rectangle(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
 
+    public double calculateArea() {
+        return length * breadth;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
     }
 }
