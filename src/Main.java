e9245ce3641d85c0a1d5458cfc34edc30911cce4
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-09-05 08:22:35 +0530

    adding Student.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        long rollno = input.nextLong();
+        Student student = new Student(name, rollno);
+    }
+}
