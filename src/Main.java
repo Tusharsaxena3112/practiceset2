568de866665b7f29e7ff641fca7a8e29d130ca2a
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-09-05 08:36:13 +0530

    adding final output

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String name = input.nextLine();
-        long rollno = input.nextLong();
-        Student student = new Student(name, rollno);
-        System.out.println(student);
+//Ques1        String name = input.nextLine();
+//        long rollno = input.nextLong();
+//        Student student = new Student(name, rollno);
+//        System.out.println(student);
+
+        //Question2
+        double length = input.nextDouble();
+        double breadth = input.nextDouble();
+        Rectangle rectangle = new Rectangle(length, breadth);
+        rectangle.calculateArea();
     }
 }
