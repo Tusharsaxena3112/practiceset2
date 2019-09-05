6012c926999a5baccc9386357c1ca0f946363ee1
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-09-05 08:44:49 +0530

    adding question4/Main.java

--- src/question4/Student.java
+++ src/question4/Student.java
@@ -4,4 +4,17 @@ public class Student {
     private String name;
     private double marks;
 
+    public Student() {
+        this.name = "unknown";
+        this.marks = 0.0;
+    }
+
+    public Student(String name, double marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public String toString() {
+        return "Name => " + name + "\nMarks => " + marks;
+    }
 }
