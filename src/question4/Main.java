e2087f3bba34cc1fb3b58f490ec436b319f92c40
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-09-05 08:47:05 +0530

    adding final output

--- src/question4/Main.java
+++ src/question4/Main.java
@@ -2,6 +2,8 @@ package question4;
 
 public class Main {
     public static void main(String[] args) {
-
+//        Student student =new Student();
+        Student student = new Student("Tushar", 100);
+        System.out.println(student);
     }
 }
